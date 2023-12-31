2eaf512eb6422ba484e36cda2e70acb3fa5ab885
Poojabg487 <135731717+Poojabg487@users.noreply.github.com>
2023-12-31 21:22:30 +0530

    Add files via upload

---
+++ Student/src/main/java/com/gl/StudentManagement/Student/security/GLWebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.gl.StudentManagement.Student.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.gl.StudentManagement.Student.model.User;
+import com.gl.StudentManagement.Student.repository.UserRepository;
+
+@Service
+public class GLWebSecurityConfig implements UserDetailsService {
+
+	   @Autowired
+	    private UserRepository userRepository;
+	     
+	    @Override
+	    public UserDetails loadUserByUsername(String username)
+	            throws UsernameNotFoundException {
+	        User user = userRepository.getUserByUsername(username);
+	         
+	        if (user == null) {
+	            throw new UsernameNotFoundException("Could not find user");
+	        }
+	         
+	        return new MyUserDetails(user);
+	    }
+
+}
